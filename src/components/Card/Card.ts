646e419790eef96d3b41ed3416c5e21bf3a87a4d
laura-barrera-valdes <laurabv99@gmail.com>
2023-03-30 12:58:23 -0500

    feat: css

--- src/components/Card/Card.ts
+++ src/components/Card/Card.ts
@@ -45,12 +45,20 @@ class AppCard extends HTMLElement {
   render() {
     if (this.shadowRoot) {
       this.shadowRoot.innerHTML = `
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="./src/components/Card/Card.css">
-      <div class= "card">
-      <h1>${this.name}</h1>
-      <h1>${this.gender}</h1>
-      <h1>${this.species}</h1>
-      </div>
+      <section class= "card">
+        <p>Name:</p>
+        <h1>${this.name}</h1>
+        </n>
+        <p>Gender:</p>
+        <h2>${this.gender}</h2>
+        <br>
+        <p>Species:</p>
+        <h2>${this.species}</h2>
+      </section>
       `;
       const css = this.ownerDocument.createElement("style");
       css.innerHTML = styles;
